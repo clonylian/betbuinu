2c791dbc94fce275162311cac6ac98bcfc5fec44
wenjieli <2773015163@qq.com>
2023-11-13 09:49:03 +0800

    first

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,538 @@
+<template>
+  <div class="hello">
+    <div class="hander">
+      <div style="margin-left: 30px;">
+        <h2>$CRYPTO</h2>
+      </div>
+      <ul class="hander_li">
+        <li style="color:  rgb(157, 69, 252);">Home</li>
+        <li >swap</li>
+        <li>Community</li>
+        <li>Find Us On</li>
+      </ul>
+      <div style="margin-right: 80px;"><span class="wallt"  style=" width: 280px;  display: block; height: 50px; line-height: 50px;   margin-top: 10px; border-radius: 30px;  color: white; width: 150px; background-color: rgb(157, 69, 252);"><a href="https://dexscreener.com/ethereum/0x6cea05f7cb348d48a0bdf86889040f6a5bae98dd" target="_blank"  style="color: white;" >VIEW CHART</a></span></div>
+    </div>
+    <div class="conext_box">
+      <div class="context_p">
+        <p class="txt_p"> BitcoinEthereumTetherBNBXRPUSDCDogecoinCardanoSolanaTron...Inu</p>
+        <p class="txt_span">We are a memecoin venture with a humorous title and ironic memes centered around degens. Come
+          onboard and let's
+          embark on a shared journey into the realm of cryptocurrencies!</p>
+        <p class="txt_join"> <span><a @click="add" style="color: white;">CONNECT</a></span></p>
+      </div>
+      <div style="width: 39%; height: 100%; margin-top: 50px;">
+        <img src="../../img/eth.webp">
+      </div>
+    </div>
+    <div class="txt_head">
+      <span>OUR COMMUNITY IS...</span>
+      <p><b>Taking Your Crypto Game To The Next Level</b></p>
+      <div class="txt_nr">
+        <div class="nr_li">
+          <img src="../../img/pepe.webp" alt="">
+          <p class="nr_biaot"><b>Crypto Education</b></p>
+          <p class="nr_wenzi">Our experts provide in-depth knowledge about memecoins and other cryptocurrencies. We offer
+            courses,
+            workshops and educational material to help you understand the market better.</p>
+        </div>
+        <div class="nr_litwo">
+          <img src="../../img/download.webp" alt="">
+          <p class="nr_biaot"><b>Portfolio Management</b></p>
+          <p class="nr_wenzi_x">Our team of experienced traders can help you manage your cryptocurrency portfolio and
+            maximize profits. We offer personalized investment plans and strategies.</p>
+        </div>
+        <div class="nr_li">
+          <img src="../../img/sw.webp" alt="">
+          <p class="nr_biaot"><b>Crypto Consulting</b></p>
+          <p class="nr_wenzi">If you are new to the world of cryptocurrencies or need help with a specific issue, we offer
+            consulting services to guide you through the process.</p>
+        </div>
+      </div>
+    </div>
+    <div class="msk_black">
+      <h1>OUR TOKENOMICS</h1>
+      <div class="msk_lb">
+        <img src="../../img/ku.webp">
+        <div class="msk_jsq">
+          <p class="msk_p">
+            <span><b>0%</b></span>.<span><b>2% </b></span>.<span><b>1M</b></span>.<span><b>LP</b></span>
+          </p>
+          <p class="msk_ptwo"><span>TAX</span><span>MAX WALLET</span><span>SUPPLY</span><span>BURNED !</span></p>
+        </div>
+        <img src="../../img/cho.webp">
+      </div>
+    </div>
+    <div class="mgl">
+      <div class="mgl_txt">
+        <span>ABOUT US</span>
+        <p class="mgl_p"><b>Explore the World of Crypto with Us</b></p>
+        <p class="mgl_ptxt">We are a group of crypto enthusiasts who believe in the power of decentralization and the
+          potential of blockchain technology. Our mission is to spread awareness about cryptocurrencies and help people
+          navigate the complex world of crypto.</p>
+        <p class="mgl_redio"><a href="https://t.me/BETBUInu" target="_blank" style="color: white;">JOIN OUR TELEGRAM</a></p>
+        <p class="mgl_redio"><a href="https://twitter.com/BETBUInu" target="_blank" style="color: white;">FOLLOW US ON TWITTER</a></p>
+      </div>
+      <div class="mgl_img">
+        <img src="../../img/mg.webp">
+      </div>
+    </div>
+    <div class="god">
+      <img class="god_imgtwo" src="../../img/msk.webp">
+      <div>
+        <p class="god_imgtwotxt"><b>
+            "I find that oftentimes the most ironic and entertaining outcome is the most likely. And the most entertaining
+            outcome would be Dogecoin becoming the global new earth currency."</b></p>
+        <span style="color: white; margin-top: 30px; display:block;"><b>¬ Elon Musk</b></span>
+      </div>
+      <img class="god_img" src="../../img/god.webp">
+    </div>
+    <div class="button_x">
+      <p> <b>You Can Find Us On:</b> </p>
+      <div class="button_xian"></div>
+      <div class="button_one">
+        
+        <div class="button_radio"><a href="https://etherscan.io/token/0x586a7cfe21e55ec0e24f0bfb118f77fe4ca87bab" target="_blank" style="color: white;">ETHERSCAN</a></div>
+        <div class="button_radio"><a href="https://www.dextools.io/app/en/ether/pair-explorer/0x6cea05f7cb348d48a0bdf86889040f6a5bae98dd" target="_blank" style="color: white;">DEXTOOLS</a></div>
+        <div class="button_radio"><a href="https://dexscreener.com/ethereum/0x6cea05f7cb348d48a0bdf86889040f6a5bae98dd" target="_blank" style="color: white;">DEXSCREENER</a></div>
+      </div>
+    </div>
+    <div class="">
+      <p style="width: 1000px; margin: 20px auto; text-align: center; word-wrap: break-word;"> 
+BitcoinEthereumTetherBNBXRPUSDCDogecoinCardanoSolanaTronMaticLitecoinPolkadotShibaInuDaiBitcoinCashWrappedBitcoinAvaxTonLinkStellarLEOUNITUSDAtomMoneroOKBEthereumClassicHederaInternetComputerFilecoinMantleLidoDaoAptosArbitrumCronosVechainNEARQuantOptismMakerTheGraphAaveXDCNetworkAlgorandTheSandboxAxieInfinityMultuversXStacksTezosUSDDThetaNetworkBitcoinSVManaSynthetixInjectiveApeCoinFantomNeiRendereCashFlowKavaChilizKucoinTokenGalaCurveDaoTOkenPaxDollarRocketPoolKlaytnZcashPaxGoldIOTATetherGoldFraxShareTerraClassicCasperGMXPepeBitTorrentCompoundMinaSuiHuobiTokenGateTokenTrustWalletTokenBoneDashNEXOGeminiDollarArweaveThorChaindYdXWooNetworkZiliqaCAKEInu © 2023
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  methods:{
+    add(){
+      // eslint-disable-next-line
+      main()
+    }
+  }
+}
+</script>
+<style >
+* {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+.hander {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  height: 70px;
+  line-height: 70px;
+  position: fixed;
+  z-index: 2;
+  background-color: white;
+  border: 1px solid rgba(0, 0, 0, 0) solid;
+}
+
+.hander_li {
+  display: flex;
+}
+
+.hander_li>li {
+  margin: 0px 12px;
+}
+
+.conext_box {
+  /* width: 100%; */
+  height: 100vh;
+  background-image: url('../../img/bg_1.png');
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+  padding: 100px 0 0 0;
+  display: flex;
+  justify-content: space-between;
+}
+
+.context_p {
+  width: 800px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  display: inline-block;
+}
+
+.txt_p {
+  font-size: 90px;
+  width: 800px;
+  word-wrap: break-word;
+  height: 400px;
+  margin: 20px 100px 30px;
+  text-align: left;
+  font-weight: 600;
+}
+
+.txt_span {
+  width: 670px;
+  font-size: 30px;
+  /* word-wrap: break-word; */
+  margin: 20px 100px 30px;
+  text-align: left;
+  line-height: 40px
+}
+
+.txt_join {
+  width: 180px;
+  height: 50px;
+  font-size: 20px;
+  margin-left: 100px;
+  color: white;
+  border-top-left-radius: 59px;
+  border-top-right-radius: 63px;
+  border-bottom-left-radius: 67px;
+  border-bottom-right-radius: 60px;
+  line-height: 50px;
+  background-color: rgb(157, 69, 252);
+}
+
+.txt_head {
+  height: 80vh;
+  /* background-color: aqua; */
+  padding-top: 100px;
+}
+
+.txt_head>span {
+  font-weight: 300;
+  color: rgb(158, 71, 252);
+}
+
+.txt_head>p {
+  margin-top: 30px;
+  margin-bottom: 30px;
+  font-size: 40px;
+  font-weight: 500;
+}
+
+.txt_nr {
+  display: flex;
+  justify-content: space-around;
+}
+
+.nr_li {
+  flex-direction: column;
+  display: flex;
+}
+
+.nr_li>img {
+  width: 250px;
+  height: 250px;
+  margin: 20px auto;
+}
+
+.nr_litwo>img {
+  width: 350px;
+  height: 200px;
+  margin: 40px auto;
+}
+
+.nr_wenzi_x {
+  margin-top: 20px;
+  line-height: 1.5em;
+  text-align: center;
+  font-size: 18px;
+  width: 320px;
+}
+
+.nr_wenzi {
+  margin-top: 15px;
+  line-height: 1.5em;
+  text-align: center;
+  font-size: 18px;
+  width: 320px;
+}
+
+.msk_black {
+  /* display: flex; */
+  height: 55vh;
+  background-color: rgb(21, 23, 63);
+  margin-top: 100px;
+}
+
+.msk_black>h1 {
+  margin-top: 80px;
+  font-size: 40px;
+  display: inline-block;
+  color: white;
+
+}
+
+.msk_lb {
+  display: flex;
+  justify-content: space-around;
+}
+
+.msk_lb>img {
+  width: 200px;
+}
+
+.msk_jsq {
+  width: 1000px;
+  display: flex;
+  color: white;
+  flex-direction: column;
+}
+
+.msk_p {
+  font-size: 60px;
+  display: flex;
+  justify-content: space-around;
+  margin: 60px 0 50px 0;
+}
+
+.msk_ptwo {
+  width: 1100px;
+  position: relative;
+  /* left: 10px; */
+  right: 32px;
+  font-size: 20px;
+  display: flex;
+  justify-content: space-around;
+  text-align: center;
+}
+
+.mgl {
+  height: 80vh;
+  /* background-image: url('../../img/bg_2.png');
+  background-repeat: no-repeat; */
+  display: flex;
+  /* background-size: 100%; */
+}
+
+.mgl_txt {
+  width: 50%;
+  background-color: white;
+  margin-top: 60px;
+  margin-left: 200px;
+}
+
+.mgl_txt>span {
+  color: rgb(158, 71, 252);
+  width: 350px;
+  font-size: 16px;
+  display: block;
+  text-align: left;
+}
+
+.mgl_p {
+  width: 350px;
+  font-size: 40px;
+  text-align: left;
+  /* background-color: aqua; */
+  margin: 30px 0 30px 0;
+
+}
+
+.mgl_ptxt {
+  width: 450px;
+  font-size: 20px;
+  font-weight: 400;
+  text-align: left;
+  margin-bottom: 40px;
+  line-height: 30px;
+}
+
+.mgl_redio {
+  width: 400px;
+  height: 50px;
+  font-size: 18px;
+  line-height: 50px;
+  color: white;
+  text-align: center !important;
+  border-top-left-radius: 59px;
+  border-top-right-radius: 63px;
+  border-bottom-left-radius: 67px;
+  border-bottom-right-radius: 60px;
+  background-color: rgb(157, 69, 252);
+  margin-top: 25px;
+}
+
+.mgl_txt>p {
+  text-align: left;
+}
+
+.mgl_img {
+  width: 50%;
+  background-image: url('../../img/bg_2.png');
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+}
+
+.mgl_img>img {
+  width: 450px;
+  height: 350px;
+  margin-top: 160px;
+}
+
+.god {
+  height: 85vh;
+  display: flex;
+  justify-content: space-between;
+  overflow: hidden;
+  background-color: rgb(157, 69, 252);
+}
+
+.god>img {
+  /* margin-top: 180px; */
+  position: relative;
+  top: 180px;
+}
+
+.god>div>p {
+  width: 740px;
+  font-size: 50px;
+  line-height: 70px;
+  font-weight: 400;
+  margin-top: 160px;
+  color: white;
+}
+
+.god>div>p span {
+  color: white;
+}
+
+.god_imgtwo {
+  height: 600px;
+  margin-left: -55px;
+}
+
+.god_img {
+  height: 600px;
+  margin-right: -105px;
+}
+
+.button_x {
+  height: 30vh;
+  border: 1px rgb(255, 255, 255) solid;
+  background-color: rgb(248, 240, 255);
+
+}
+
+.button_xian {
+  width: 100%;
+  height: 2px;
+  background-color: white;
+}
+
+.button_x>p {
+  font-size: 18px;
+  padding-top: 60px;
+  margin-bottom: 10px;
+}
+
+.button_one {
+  margin: 30px auto;
+  display: flex;
+  width: 1000px;
+  height: 80px;
+  line-height: 40px;
+  justify-content: space-around;
+}
+
+.button_radio {
+  width: 200px;
+  height: 50px;
+  line-height: 50px;
+  border-top-left-radius: 55px;
+  border-top-right-radius: 58px;
+  border-bottom-left-radius: 59px;
+  border-bottom-right-radius: 52px;
+  color: white;
+  background-color: rgb(157, 69, 252);
+  margin-top: 40px;
+}
+.button_radio:hover{
+  background-color: rgb(21, 23, 63);
+  border: 1px blue solid;
+}
+.mgl_redio:hover{
+  background-color: rgb(21, 23, 63) !important;
+  border: 1px blue solid; 
+}
+.txt_join:hover{
+  background-color: rgb(21, 23, 63) !important;
+  border: 1px blue solid; 
+}
+.wallt:hover{
+  background-color: rgb(21, 23, 63) !important;
+  border: 1px blue solid; 
+}
+.hello{
+  width: 100%;
+  height: 100%;
+}
+@keyframes move-up {
+  0% {
+    transform: translateY(100%);
+    opacity: 0;
+  }
+  /* 25% {
+    transform: translateY(75%);
+    opacity: 0.2;
+  }
+  50% {
+    transform: translateY(50%);
+    opacity: 0.4;
+  }
+ 75% {
+    transform: translateY(25%);
+    opacity: 0.8;
+  } */
+  100% {
+    transform: translateY(0);
+    opacity: 1;
+  }
+}
+
+.context_p {
+  animation: move-up 2s ease-in-out;
+}
+
+@keyframes move-upday {
+  0% {
+    transform: translateY(100%);
+    opacity: 0;
+  }
+  /* 25% {
+    transform: translateY(75%);
+    opacity: 0.2;
+  }
+  50% {
+    transform: translateY(50%);
+    opacity: 0.4;
+  }
+75% {
+    transform: translateY(25%);
+    opacity: 0.8; */
+  /* } */
+  100% {
+    transform: translateY(0);
+    opacity: 1;
+  }
+}
+.txt_head{
+  animation: move-upday 2s ease-in-out;
+}
+.msk_jsq{
+  animation: move-upday 3s ease-in-out;
+}
+.mgl_redio{
+
+  animation: move-upday 4s ease-in-out;
+}
+.god_imgtwotxt{
+  animation: move-upday 4s ease-in-out;
+}
+
+</style>
